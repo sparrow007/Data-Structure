89583e74031b0f31acedd04a73fbbd4f1fe56dda
Sparrow007 <ankit.steven007@gmail.com>
2020-08-25 10:50:54 +0530

    add stack codes

--- src/programs/Stack/NextGreaterElement.java
+++ src/programs/Stack/NextGreaterElement.java
@@ -28,28 +28,60 @@ public class NextGreaterElement {
         display(nge);
     }
 
-    public static int[] solve(int[] arr){
-        // solve
+
+    // Solve the stack from front to end
+    public static int[] solve(int[] arr, int l){
+
+        int[] nge = new int[arr.length];
+
         Stack<Integer> st = new Stack<>();
+        st.push(0);
+
+        for(int i = 1; i < arr.length; i++) {
+
+            while(st.size() > 0 && arr[i] > arr[st.peek()]) {
+                 nge[st.peek()] = arr[i];
+                 st.pop();
+            }
+
+            st.push(i);
+        }
+
+        int length = st.size();
+        for (int i = 0; i < length; i++) {
+            nge[st.peek()] = -1;
+            st.pop();
+        }
+
+        return nge;
+    }
+
+    // Solve the problem from end to start so theat next time we can't be setting this type of
+    public static int[] solve(int[] arr) {
+
+        int[] nge = new int[arr.length];
+
+        Stack<Integer> st = new  Stack<Integer>();
         st.push(arr[arr.length-1]);
-        arr[arr.length-1] = -1;
+        nge[arr.length-1] = -1;
 
-        for(int i = arr.length - 2; i >= 0; i--) {
+        for (int i = arr.length-2; i >= 0 ; i--) {
 
-            while (st.size() != 0 && arr[i] >= st.peek()) {
+            while( st.size() > 0 && arr[i] > st.peek()) {
                 st.pop();
             }
 
             if(st.size() == 0) {
-                arr[i] = -1;
+                nge[i] = -1;
             }
-            else if(arr[i] < st.peek()) {
-                arr[i] = st.peek();
-            }
+            else
+                nge[i] = st.peek();
 
             st.push(arr[i]);
         }
 
-        return null;
+        return nge;
     }
+
+
 }
