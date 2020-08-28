2ee26599022d0d114abe4fdc432a0fac6171a255
Sparrow007 <ankit.steven007@gmail.com>
2020-08-28 10:55:01 +0530

    add more codes

---
+++ src/programs/LinkList/MergePointOfList.java
@@ -0,0 +1,58 @@
+package programs.LinkList;
+
+public class MergePointOfList {
+    static class SinglyLinkedListNode {
+
+        public SinglyLinkedListNode(int data) {
+            this.data = data;
+        }
+
+        int data;
+        SinglyLinkedListNode next;
+    }
+
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+        int firstSize = 0;
+        int secondSize = 0;
+
+        SinglyLinkedListNode tempA = head1;
+        SinglyLinkedListNode tempB = head2;
+
+
+        while(tempA != null) {
+            firstSize++;
+            tempA = tempA.next;
+        }
+
+        while(tempB != null) {
+            secondSize++;
+            tempB = tempB.next;
+        }
+
+        tempA = head1;
+        tempB = head2;
+
+        if(firstSize > secondSize) {
+            for (int i = 0; i < firstSize - secondSize; i++) {
+                tempA = tempA.next;
+            }
+        }else {
+            for (int i = 0; i < secondSize - firstSize; i++)
+                tempB = tempB.next;
+        }
+
+        while(tempA != null && tempB != null) {
+
+            if(tempA == tempB) {
+                return tempA.data;
+            }
+
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+
+        return -1;
+
+    }
+}
