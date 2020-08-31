722a16655136b367cd2207ed11028fc859022441
Sparrow007 <ankit.steven007@gmail.com>
2020-09-01 02:36:13 +0530

    add codes

--- src/programs/Extraa/LoopExp.java
+++ src/programs/Extraa/LoopExp.java
@@ -1,21 +1,69 @@
 package programs.Extraa;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LoopExp {
 
     public static void main(String args[]) {
 
-        Scanner scanner  = new Scanner(System.in);
-        int a = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int test = scanner.nextInt();
 
-        int sum = 0;
+        while(test > 0) {
 
-        while(a != 0) {
-            sum += a % 10;
-            a = a/10;
+            int size1 = scanner.nextInt();
+            int size2 = scanner.nextInt();
+            int size3 = scanner.nextInt();
+
+            HashMap<Integer, Integer> res = new HashMap<>();
+
+            for (int i = 0; i < size1; i++)  {
+                int temp = scanner.nextInt();
+                res.put(temp, 1);
+            }
+
+            for (int i = 0; i < size2; i++) {
+                int temp  = scanner.nextInt();
+                if(res.containsKey(temp)) {
+                    int value = res.get(temp);
+                    if (value == 1)
+                       res.put(temp, value + 1);
+                }
+            }
+
+            for (int i = 0; i < size3; i++) {
+                int temp  = scanner.nextInt();
+                if(res.containsKey(temp)) {
+                    int value = res.get(temp);
+                    if (value == 2)
+                        res.put(temp, value + 1);
+                }
+            }
+
+            TreeMap<Integer, Integer> sorted = new TreeMap<>();
+
+            // Copy all data from hashMap into TreeMap
+            sorted.putAll(res);
+
+            // Display the TreeMap which is naturally sorted
+            boolean isAllEqaul = false;
+            for (Map.Entry<Integer, Integer> entry : sorted.entrySet()) {
+                 if(entry.getValue() == 3) {
+                    isAllEqaul = true;
+                    System.out.print(entry.getKey() + " ");
+                 }
+            }
+
+            System.out.println(sorted);
+
+
+            if(!isAllEqaul)
+                System.out.println(-1);
+            else
+
+
+            test--;
         }
 
-        System.out.println(sum);
     }
 }
