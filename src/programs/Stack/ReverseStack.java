73a2dc72777e65179c4df1a292b2bceeceb6128c
Sparrow007 <ankit.steven007@gmail.com>
2020-09-10 22:14:06 +0530

    add stack codes

--- src/programs/Stack/ReverseStack.java
+++ src/programs/Stack/ReverseStack.java
@@ -6,6 +6,39 @@ public class ReverseStack {
 
     private static Stack<Integer> st = new Stack<>();
 
+    static void sortAtBottom(int x) {
+
+        if (st.isEmpty()) {
+            System.out.println(x);
+            st.push(x);
+        }else {
+
+            int pop = st.pop();
+
+            if(x < pop)
+              sortAtBottom(x);
+            else {
+                st.push(pop);
+                st.push(x);
+                return;
+            }
+              st.push(pop);
+        }
+
+    }
+
+    public static void sort()
+    {
+        if (st.size() > 0) {
+
+            int x = st.pop();
+            sort();
+
+            sortAtBottom(x);
+
+        }
+    }
+
     static void insertAtBottom(int x) {
 
         if(st.isEmpty()) st.push(x);
@@ -47,15 +80,22 @@ public class ReverseStack {
 
     }
 
+
+
     public static void main(String[] args) {
 
         st.push(10);
-        st.push(20);
+        st.push(70);
         st.push(30);
-        st.push(40);
-        st.push(50);
+        st.push(20);
+        st.push(80);
         System.out.println(st);
-        reverse();
-        System.out.println(st);
+        sort();
+
+        int size = st.size();
+        for (int i = 0; i < size; i++) {
+            System.out.print(st.pop() + " ");
+        }
+
     }
 }
