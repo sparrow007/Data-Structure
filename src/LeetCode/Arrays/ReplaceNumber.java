9c75b263761879124c76054246f72e0fcfc056ed
Sparrow007 <ankit.steven007@gmail.com>
2020-09-18 16:43:56 +0530

    add array codes

--- src/LeetCode/Arrays/ReplaceNumber.java
+++ src/LeetCode/Arrays/ReplaceNumber.java
@@ -1,5 +1,7 @@
 package LeetCode.Arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ReplaceNumber {
@@ -11,8 +13,40 @@ public class ReplaceNumber {
 
         for (int i = 0; i < length; i++) arr[i] = scanner.nextInt();
 
-        int[] b = replaceElements(arr);
-        for (int i = 0; i < length; i++) System.out.print(b[i] + " ");
+        int k = scanner.nextInt();
+
+        int b = findPairs(arr, k);
+
+        System.out.println(b);
+    }
+
+    public static int findPairs(int[] nums, int k) {
+
+        int count = 0;
+
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                int diff = Math.abs(nums[i] - nums[j]);
+                
+                if (diff == k){
+                    if (hmap.containsKey(nums[i]) && hmap.containsValue(nums[j])) {
+
+                    }else {
+                        hmap.put(nums[i], nums[j]);
+                        count++;
+                    }
+                    //System.out.println("diff " + diff + " i = " + i + " j = " + j);
+
+                }
+            }
+        }
+
+        return count;
+
     }
 
     public static int[] replaceElements(int[] arr) {
