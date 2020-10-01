0256b9712e8247544a153979385c7fe8d5c6e281
Sparrow007 <ankit.steven007@gmail.com>
2020-10-02 02:00:56 +0530

    add codes

--- src/programs/GenericTree/GenericTreeStatic.java
+++ src/programs/GenericTree/GenericTreeStatic.java
@@ -6,14 +6,13 @@ import java.util.Stack;
 public class GenericTreeStatic {
 
     static int min;
-    static  int max;
     static int height;
     static int size;
     //Node of the tree and it's data with it's next childrens
     static class Node {
 
         int data;
-        ArrayList<GenericTreeConstructor.Node> childrens;
+        ArrayList<Node> childrens;
 
         Node(int data) {
             this.data = data;
@@ -21,19 +20,43 @@ public class GenericTreeStatic {
         }
     }
 
+    static int max = Integer.MIN_VALUE;
+    static int max_node = 0;
+
+    static int sum(Node node) {
+
+        int childSum = 0;
+
+        if(node.childrens.size() == 0) {
+            childSum =  node.data;
+        }
+
+
+        for (Node child : node.childrens) {
+            childSum += sum(child);
+        }
+
+        if(childSum > max) {
+            max_node = node.data;
+            max = childSum;
+        }
+        System.out.println("childSum " + childSum + " node " + node.data);
+        return childSum;
+    }
+
     public static void main(String[] args) {
 
-        int arr[] = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1,120, -1,-1,90, -1,-1,40
-                ,100,-1, -1,-1};
+        int arr[] = { 10, 20, -50, -1, 60, -1, -1, 30, -70, -1, 80, 110, -1,120, -1,-1,90, -1,-1,40
+                ,-100,-1, -1,-1};
 
-        Stack<GenericTreeConstructor.Node> st = new Stack<>();
-        GenericTreeConstructor.Node root = new GenericTreeConstructor.Node(arr[0]);
+        Stack<Node> st = new Stack<>();
+        Node root = new Node(arr[0]);
         st.push(root);
 
         for (int i = 1; i < arr.length; i++ ) {
 
             if (arr[i] != -1) {
-                GenericTreeConstructor.Node node = new GenericTreeConstructor.Node(arr[i]);
+                Node node = new Node(arr[i]);
                 st.peek().childrens.add(node);
                 st.push(node);
             }else {
@@ -46,7 +69,10 @@ public class GenericTreeStatic {
         max = Integer.MIN_VALUE;
         height = 0;
 
+        sum(root);
 
+        System.out.println(max);
+        System.out.println(max_node);
 
         //System.out.println(max(root));
 
