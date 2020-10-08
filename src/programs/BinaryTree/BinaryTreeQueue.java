8cfedec50998b5abac3fd4d6ed2959a20d9e0fde
Sparrow007 <ankit.steven007@gmail.com>
2020-10-08 23:07:00 +0530

    add tree insertion

---
+++ src/programs/BinaryTree/BinaryTreeQueue.java
@@ -0,0 +1,23 @@
+package programs.BinaryTree;
+
+public class BinaryTreeQueue {
+
+    static class Node {
+
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data) {
+            this.data = data;
+            right = left = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
