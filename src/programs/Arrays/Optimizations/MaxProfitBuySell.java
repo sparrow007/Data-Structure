262a9e66748dde07c5f05712c73e1abd23104ab0
Sparrow007 <ankit.steven007@gmail.com>
2020-10-23 23:12:59 +0530

    add profit buy sell

--- src/programs/Arrays/Optimizations/MaxProfitBuySell.java
+++ src/programs/Arrays/Optimizations/MaxProfitBuySell.java
@@ -6,7 +6,40 @@ public class MaxProfitBuySell {
 
        int price[] = {10, 22, 5, 75, 65, 80};
 
-       profit(price, price.length);
+       maxProfit(price, price.length);
+
+    }
+
+    static void maxProfit(int price[], int n) {
+
+        int[] profit = new int[n];
+
+        int max_price= price[n-1];
+
+        //Right to left traversal and store the maximum in one trasaction
+
+
+        for (int i = n-2; i >= 0; i--) {
+
+            if (price[i] > max_price)
+                max_price = price[i];
+
+            profit[i] = Math.max(profit[i + 1], max_price - price[i]);
+
+        }
+        
+        int min_price = price[0];
+        for (int i = 1; i < n; i++) {
+
+            if(price[i] < min_price) {
+                min_price = price[i];
+            }
+
+            profit[i] = Math.max(profit[i-1], profit[i] + price[i] - min_price);
+
+        }
+
+        System.out.println(profit[n-1]);
 
     }
 
@@ -22,5 +55,7 @@ public class MaxProfitBuySell {
 
         System.out.println(maxProfit + " is high profit ");
     }
+
+
 
 }
