2d02fc3182f5fec6e99d14fc91b3745b4a44852c
Sparrow007 <ankit.steven007@gmail.com>
2020-11-14 10:44:15 +0530

    kotlin object example

--- src/kotlinLearning/KotlinObjectExp.kt
+++ src/kotlinLearning/KotlinObjectExp.kt
@@ -4,4 +4,18 @@ fun main() {
 
     //compiler intancetitiate for us and make it singleton
     Singleton.callMe()
+
+    //Calling the object from the class
+    OuterClass.My.callUs()
+}
+
+//Can't call the property of outer class from the innner object (you can't use the inner)
+class OuterClass {
+    val value : Int = 0
+    object My {
+        fun callUs() {
+            println("Yes the calling pattern is used")
+        }
+    }
+
 }
