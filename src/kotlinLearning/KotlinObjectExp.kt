e4dc9c86f8029d1d0335e4fa11f8ea4e739b0468
Sparrow007 <ankit.steven007@gmail.com>
2020-11-14 13:50:27 +0530

    adding the kotlin

--- src/kotlinLearning/KotlinObjectExp.kt
+++ src/kotlinLearning/KotlinObjectExp.kt
@@ -1,5 +1,7 @@
 package kotlinLearning
 
+import java.io.Writer
+
 fun main() {
 
     //compiler intancetitiate for us and make it singleton
@@ -7,6 +9,37 @@ fun main() {
 
     //Calling the object from the class
     OuterClass.My.callUs()
+
+    val runnable = object : Runnable {
+        override fun run() {
+            println("You are running a another thread")
+            Thread.sleep(4000)
+            println("You have called me afer the 4sec ")
+        }
+
+    }
+
+    //Object can be used a anonymous classes
+    Thread(runnable).run()
+
+    //You can also extends the class and implement interface with object in all the 3 situations
+
+    val writer = object : Writer() {
+        override fun write(cbuf: CharArray, off: Int, len: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun flush() {
+            TODO("Not yet implemented")
+        }
+
+        override fun close() {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+
 }
 
 //Can't call the property of outer class from the innner object (you can't use the inner)
@@ -17,5 +50,4 @@ class OuterClass {
             println("Yes the calling pattern is used")
         }
     }
-
 }
