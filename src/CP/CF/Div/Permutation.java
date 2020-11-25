1b7d5244187fcd33a5c279befa9f774b088fc07e
Sparrow007 <ankit.steven007@gmail.com>
2020-11-26 00:48:42 +0530

    add codeforces

--- src/CP/CF/Div/Permutation.java
+++ src/CP/CF/Div/Permutation.java
@@ -6,25 +6,51 @@ public class Permutation {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
 
-        long a = scanner.nextLong();
-        long b = scanner.nextLong();
+       long a = scanner.nextLong();
+       long b = scanner.nextLong();
 
-        String s = String.valueOf(a);
+       int[] first = new int[10];
+       int[] second = new int[10];
 
-        while (b != 0) {
-            int r = (int) (b % 10);
-            b = b / 10;
-            if (!s.contains(String.valueOf(r))) {
-                System.out.println("NO");
-                return;
-            }
+        long diff = b - a;
 
+        if (diff < 0) {
+            System.out.println("NO");
+            return;
         }
 
-        System.out.println("YES");
+        String firstValue = String.valueOf(a);
+        String secondValue = String.valueOf(diff);
+
+        while (a != 0) {
+            int r = (int) (a % 10);
+            a = a / 10;
+            first[r]++;
+        }
+
+        while (diff != 0) {
+            int r = (int) (diff % 10);
+            diff = diff / 10;
+            second[r]++;
+        }
+
+
+        if (firstValue.length() == secondValue.length()) {
+
+            for (int i = 0; i < 10; i++) {
+                if (first[i] != second[i]) {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+
+            System.out.println("YES");
+
+        }else {
+            System.out.println("NO");
+        }
 
     }
-
 }
