b106af0651c78c8c8ce7a98134a4a7e34094b7ae
Sparrow007 <ankit.steven007@gmail.com>
2020-12-23 00:49:40 +0530

    adding codes

---
+++ src/programs/Recursions/revision/PowerLinear.java
@@ -0,0 +1,25 @@
+package programs.Recursions.revision;
+
+import java.util.Scanner;
+
+public class PowerLinear {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc= new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println(power(x, n));
+    }
+
+    public static int power(int x, int n){
+        if (n == 0) return 1;
+
+
+        if (n % 2 == 0) return x * x * power(x,n - 2);
+        else return x * power(x, n - 1);
+
+    }
+
+}
