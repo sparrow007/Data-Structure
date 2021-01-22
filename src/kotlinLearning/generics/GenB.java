158ebe65612856abdf688c96984d06c26616adf1
Sparrow007 <ankit.steven007@gmail.com>
2021-01-23 00:31:34 +0530

    adding values

--- src/kotlinLearning/generics/GenB.java
+++ src/kotlinLearning/generics/GenB.java
@@ -6,22 +6,12 @@ import java.util.Stack;
 
 public class GenB {
 
-    static <T> void pass (GenA<T> s) {
+    static <T extends Number> void pass (GenA<T> s) {
         System.out.println(s.getValue() + "");
     }
 
     public static void main(String[] args) {
-        pass(new GenA<>(20));
-
-        Integer[] ar = new Integer[9];
-        Number[] arr = new Number[1];
-        arr = ar;
-
-        List<Number> list = new ArrayList<>();
-        List<? extends Integer> l = new ArrayList<>();
-        l.clear();
-
-        Stack<Number> stack = new Stack<>();
+        pass(new GenA<>(5));
 
     }
 
