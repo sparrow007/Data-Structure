3974ac6e51f472e2e3d6b42eec8bf6b62fe4ec76
Sparrow007 <ankit.steven007@gmail.com>
2021-02-06 12:57:35 +0530

    adding update zerooneKnapSack

--- src/programs/dp/ZeroOneKnapSack.java
+++ src/programs/dp/ZeroOneKnapSack.java
@@ -39,6 +39,14 @@ public class ZeroOneKnapSack {
             }
         }
 
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
         System.out.println(dp[n][cap]);
     }
 }
