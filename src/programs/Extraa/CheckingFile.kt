ed7e2f386d2dff173c2f39c33695210ccef615b8
Sparrow007 <ankit.steven007@gmail.com>
2021-02-24 15:10:16 +0530

    adding simple calculation:

---
+++ src/programs/Extraa/CheckingFile.kt
@@ -0,0 +1,10 @@
+package programs.Extraa
+
+fun main() {
+    val first = 0
+    val last = 10
+
+    for (index in first until last) {
+        println(index)
+    }
+}
---
+++ src/programs/Extraa/Modulus.java
@@ -0,0 +1,8 @@
+package programs.Extraa;
+
+public class Modulus {
+
+    public static void main(String[] args) {
+        System.out.println((3907 % 102f)/ 102f);
+    }
+}
