2bec16e909fd9f87f2fb152bf2e08fd9afdf453a
Sparrow007 <ankit.steven007@gmail.com>
2021-03-12 11:55:02 +0530

    adding the check for modolus

--- src/programs/Extraa/CheckingFile.kt
+++ src/programs/Extraa/CheckingFile.kt
@@ -4,7 +4,5 @@ fun main() {
     val first = -1
     val last =0
 
-    val a = 1 * 1.0f / (1080.0f)
-    println("is greater than 1 ${a > 1}" )
-    println("In this ${360 * 1.0f / (1080.0f)}")
+    println(-1 % 6)
 }
