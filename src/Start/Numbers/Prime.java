c48e1be5d443dca1290e97d8ac0ea82e7f1d0e96
Sparrow007 <ankit.steven007@gmail.com>
2021-05-29 17:38:40 +0530

    adding few

--- src/Start/Numbers/Prime.java
+++ src/Start/Numbers/Prime.java
@@ -9,22 +9,37 @@ public class Prime {
         int t = scn.nextInt();
         while (t-- > 0) {
             int n = scn.nextInt();
-            int flag = 0;
-            if (n == 1) {
-                System.out.println("prime");
-                continue;
-            }
-            for (int i = 2; i <= n/2; i++) {
-                if (n % i == 0) {
-                    System.out.println("not prime");
-                    flag = 1;
-                    break;
-                }
-            }
-
-            if (flag == 0) {
-                System.out.println("prime");
-            }
+            System.out.println(isPrime(n));
+//            int flag = 0;
+//            if (n == 1) {
+//                System.out.println("prime");
+//                continue;
+//            }
+//            for (int i = 2; i <= n/2; i++) {
+//                if (n % i == 0) {
+//                    System.out.println("not prime");
+//                    flag = 1;
+//                    break;
+//                }
+//            }
+//
+//            if (flag == 0) {
+//                System.out.println("prime");
+//            }
         }
+    }
+
+
+    private static boolean isPrime(int n) {
+
+        if (n <= 1) return false;
+
+        if (n % 2 == 0 || n % 3 == 0 ) return false;
+
+        for (int i = 5; i <= Math.sqrt(n); i += 6) {
+            if (n % i == 0)
+        }
+
+        return true;
     }
 }
