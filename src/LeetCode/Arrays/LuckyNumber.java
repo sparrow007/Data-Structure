81af1a0e48a790e40c236c09ccc32c0ddd541252
Sparrow007 <ankit.steven007@gmail.com>
2021-07-22 20:10:12 +0530

    add new lines

--- src/LeetCode/Arrays/LuckyNumber.java
+++ src/LeetCode/Arrays/LuckyNumber.java
@@ -1,19 +1,22 @@
 package LeetCode.Arrays;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LuckyNumber {
 
     public static void main(String[] args) {
+        boolean b1 = false;
+        boolean b2 = fix(b1);
 
-        Map<Integer, Integer> map = new HashMap<>();
 
+        System.out.println(b1 + "" + b2);
+    }
 
-
-        for (int key : map.keySet()) {
-
-        }
-
+    static boolean fix(boolean b1) {
+        b1 = true;
+        return b1;
     }
 }
