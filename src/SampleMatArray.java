e89cfb51a8f42a4fe59386d7c4e3754236c0732d
Sparrow007 <ankit.steven007@gmail.com>
2022-03-24 00:23:45 +0530

    adding the sample mat array for pepcoding

---
+++ src/SampleMatArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SampleMatArray {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+        int[][] mat = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                mat[i][j] = scn.nextInt();
+            }
+        }
+    }
+}
--- src/programs/FoPattern.java
+++ src/programs/FoPattern.java
@@ -6,8 +6,35 @@ public class FoPattern {
 
 
     public static void main(String args[]) {
+      //  Scanner scanner = new Scanner(System.in);
+       // int n = scanner.nextInt();
+
+/**
+ *     Scanner scn = new Scanner(System.in);
+ *     int row = scn.nextInt();
+ *     int col = scn.nextInt();
+ *     int[][] mat = new int[row][col];
+ *
+ *     for (int i = 0; i < row; i++) {
+ *            for (int j = 0; j < col; j++) {
+ *              mat[i][j] = scn.nextInt();
+ *            }
+ *     }
+ */
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int n2 = scanner.nextInt();
+        int arr2[] = new int[n2];
+
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = scanner.nextInt();
+        }
 
         for(int i = 0; i < n; i++) {
 
